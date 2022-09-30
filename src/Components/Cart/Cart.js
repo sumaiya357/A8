f7232b660e36c67e6e75ba6a2fd96a813a973113
sumaiya357 <rahmansumaiya75@gmail.com>
2022-09-30 18:13:36 +0600

    break time and exercise time added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -7,73 +7,9 @@
  import './Cart.css'
  const Cart = () => {
     return (
-        <div className='myCart'>
-
-            {/*-----------About Me----------- */}
-           <div className='cart'>
-              <img className='img' src={img} alt="" />
-                  <div className='info'>
-                     <h3>Alexendar Herry</h3>
-                     <span><FontAwesomeIcon className='icon' icon={faArrows}></FontAwesomeIcon></span>
-                     <span>KumarPara,Sylhet</span>
-                  </div>
-           </div>
-           
-             
-              {/* ----------about me details------- */}
-               <div className='info2'>
-                  <div className='weight'>
-                     <span>65</span>kg
-                     <h6>Weight</h6>
-                     </div>
-                  <div className='height'>
-                    <span>5.5</span>
-                     <h6>Height</h6>
-                     </div>
-                  <div className='age'>
-                  <span>22</span>yrs
-                     <h6>age</h6>
-                     </div>
-               
-               </div>
-
-               {/* ----------BREAK DETAILS-------*/}
-               <div>
-                  <h3 className='break'>Break Time</h3>
-                  
-                <div className='info3'>
-                     <div className='s1'>
-                        <button>10s</button>
-                        </div>
-                     <div className='s2'>
-                     <button>20s</button>
-                        
-                        </div>
-                     <div className='s3'>
-                     <button>30s</button>
-                        </div>
-                     <div className='s4'>
-                     <button>40s</button>
-                        </div>
-                  </div>
-            </div>
-
-            {/* ------------Exercise Details----------*/}
-            <div className='exercise-details' >
-
-                 <h2>Exercise Details</h2>
-
-                <div className='info4'>
-                     <h3 className='exercise'>Exercise Time <h4> <span>0</span> seconds</h4></h3>
-                     <h3 className='break-time'>Break Time <h4> <span>0</span> seconds</h4></h3>
-                </div>
-            </div>
-
-            {/*-------- Activity complete----- */}
-            <div >
-               <button className='btn2'>Activity Completed</button>
-            </div>
-         </div>
+       <div>
+         
+       </div>
        
     );
  };
--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -4,14 +4,17 @@
   import './Item.css'
   
   const Item = (props) => {
+    const {img, name,time} =props;
     
+    console.log(props)
+  
     return (
         <div className='details2' >
-            <img  src={props.img} alt="" />
-            <h3>{props.name}</h3>
-            <p>Time required :<strong>{props.time}</strong></p>
+            <img  src={img} alt="" />
+            <h3>{name}</h3>
+            <p>Time required : <strong>{time}</strong>s</p>
 
-            <button  className='btn'>Add To List</button>
+            <button onClick={()=> {props.add(props.time)}} className='btn'>Add To List</button>
         </div>
         
     );
